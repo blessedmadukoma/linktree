4f132c89d79d3dfa3a96af5ecbcc29c9b0fc0fff
blessedmadukoma <blessedmadukoma@gmail.com>
2023-01-29 19:42:46 +0100

    centered texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,19 +13,23 @@ function LinkCard({
   return (
     <a
       href={url}
-      className="flexx items-center p-1 rounded-md w-full border border-gray-300 mb-3 hover:scale-105 transition-all bg-gray-100"
+      className="flexx items-center p-1 rounded-md w-full border border-gray-300 mb-3 hover:scale-105 transition-all bg-gray-100 max-w-3xl"
     >
       <div className="flex text-center w-full">
-        {image && (
-          <Image
-            className="rounded-md"
-            alt={title}
-            src={image}
-            width={40}
-            height={40}
-          />
-        )}
-        <h2 className="font-semibold w-full text-center text-gray-700">{title}</h2>
+        <div className="w-10">
+          {image && (
+            <Image
+              className="rounded-md"
+              alt={title}
+              src={image}
+              width={40}
+              height={40}
+            />
+          )}
+        </div>
+        <h2 className="flex justify-center items-center font-semibold w-full text-gray-700">
+          {title}
+        </h2>
       </div>
     </a>
   );
@@ -41,7 +45,9 @@ export default function Home() {
         width={96}
         height={96}
       />
-      <h1 className="font-semibold mt-4 mb-8 text-xl text-white">{data.name}</h1>
+      <h1 className="font-semibold mt-4 mb-8 text-xl text-white">
+        {data.name}
+      </h1>
       {data.links.map((link) => (
         <LinkCard key={link.url} {...link} />
       ))}
