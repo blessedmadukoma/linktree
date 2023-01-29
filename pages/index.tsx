f5c700eaa77c92360a0f4de17fa40f54f67c7c11
blessedmadukoma <blessedmadukoma@gmail.com>
2023-01-29 10:38:43 +0100

    set up project and mini db (data.json)

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
+
+export default Home
