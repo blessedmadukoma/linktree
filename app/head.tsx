91b33674dd57a2909d6b47014f1e6bffd0abf7ae
blessedmadukoma <blessedmadukoma@gmail.com>
2023-02-04 18:39:27 +0100

    added head such as title and meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head({
+ params
+}:{params: {slug: string }}) {
+ return (
+  <>
+  <title>Linktree Clone</title>
+  <meta name="description" content="Next.JS and TailwindCSS" />
+  </>
+ )
+}
